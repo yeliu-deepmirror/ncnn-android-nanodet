92f1a32bc27956148918c60d7d182b018e9ad661
yeliu <yeliu@deepmirror.com>
2024-10-22 11:45:36 +0800

    clang

--- app/src/main/jni/nanodet.h
+++ app/src/main/jni/nanodet.h
@@ -19,33 +19,34 @@
 
 #include <net.h>
 
-struct Object
-{
-    cv::Rect_<float> rect;
-    int label;
-    float prob;
+struct Object {
+  cv::Rect_<float> rect;
+  int label;
+  float prob;
 };
 
-class NanoDet
-{
-public:
-    NanoDet();
+class NanoDet {
+ public:
+  NanoDet();
 
-    int load(const char* modeltype, int target_size, const float* mean_vals, const float* norm_vals, bool use_gpu = false);
+  int load(const char* modeltype, int target_size, const float* mean_vals, const float* norm_vals,
+           bool use_gpu = false);
 
-    int load(AAssetManager* mgr, const char* modeltype, int target_size, const float* mean_vals, const float* norm_vals, bool use_gpu = false);
+  int load(AAssetManager* mgr, const char* modeltype, int target_size, const float* mean_vals,
+           const float* norm_vals, bool use_gpu = false);
 
-    int detect(const cv::Mat& rgb, std::vector<Object>& objects, float prob_threshold = 0.4f, float nms_threshold = 0.5f);
+  int detect(const cv::Mat& rgb, std::vector<Object>& objects, float prob_threshold = 0.4f,
+             float nms_threshold = 0.5f);
 
-    int draw(cv::Mat& rgb, const std::vector<Object>& objects);
+  int draw(cv::Mat& rgb, const std::vector<Object>& objects);
 
-private:
-    ncnn::Net nanodet;
-    int target_size;
-    float mean_vals[3];
-    float norm_vals[3];
-    ncnn::UnlockedPoolAllocator blob_pool_allocator;
-    ncnn::PoolAllocator workspace_pool_allocator;
+ private:
+  ncnn::Net nanodet;
+  int target_size;
+  float mean_vals[3];
+  float norm_vals[3];
+  ncnn::UnlockedPoolAllocator blob_pool_allocator;
+  ncnn::PoolAllocator workspace_pool_allocator;
 };
 
-#endif // NANODET_H
+#endif  // NANODET_H
--- app/src/main/jni/nanodetncnn.cpp
+++ app/src/main/jni/nanodetncnn.cpp
@@ -13,8 +13,8 @@
 // specific language governing permissions and limitations under the License.
 
 #include <android/asset_manager_jni.h>
-#include <android/native_window_jni.h>
 #include <android/native_window.h>
+#include <android/native_window_jni.h>
 
 #include <android/log.h>
 
@@ -23,8 +23,8 @@
 #include <string>
 #include <vector>
 
-#include <platform.h>
 #include <benchmark.h>
+#include <platform.h>
 
 #include "nanodet.h"
 
@@ -35,254 +35,212 @@
 
 #if __ARM_NEON
 #include <arm_neon.h>
-#endif // __ARM_NEON
+#endif  // __ARM_NEON
 
-static int draw_unsupported(cv::Mat& rgb)
-{
-    const char text[] = "unsupported";
+static int draw_unsupported(cv::Mat& rgb) {
+  const char text[] = "unsupported";
 
-    int baseLine = 0;
-    cv::Size label_size = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX, 1.0, 1, &baseLine);
+  int baseLine = 0;
+  cv::Size label_size = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX, 1.0, 1, &baseLine);
 
-    int y = (rgb.rows - label_size.height) / 2;
-    int x = (rgb.cols - label_size.width) / 2;
+  int y = (rgb.rows - label_size.height) / 2;
+  int x = (rgb.cols - label_size.width) / 2;
 
-    cv::rectangle(rgb, cv::Rect(cv::Point(x, y), cv::Size(label_size.width, label_size.height + baseLine)),
-                    cv::Scalar(255, 255, 255), -1);
+  cv::rectangle(rgb,
+                cv::Rect(cv::Point(x, y), cv::Size(label_size.width, label_size.height + baseLine)),
+                cv::Scalar(255, 255, 255), -1);
 
-    cv::putText(rgb, text, cv::Point(x, y + label_size.height),
-                cv::FONT_HERSHEY_SIMPLEX, 1.0, cv::Scalar(0, 0, 0));
+  cv::putText(rgb, text, cv::Point(x, y + label_size.height), cv::FONT_HERSHEY_SIMPLEX, 1.0,
+              cv::Scalar(0, 0, 0));
 
-    return 0;
+  return 0;
 }
 
-static int draw_fps(cv::Mat& rgb)
-{
-    // resolve moving average
-    float avg_fps = 0.f;
-    {
-        static double t0 = 0.f;
-        static float fps_history[10] = {0.f};
+static int draw_fps(cv::Mat& rgb) {
+  // resolve moving average
+  float avg_fps = 0.f;
+  {
+    static double t0 = 0.f;
+    static float fps_history[10] = {0.f};
 
-        double t1 = ncnn::get_current_time();
-        if (t0 == 0.f)
-        {
-            t0 = t1;
-            return 0;
-        }
-
-        float fps = 1000.f / (t1 - t0);
-        t0 = t1;
-
-        for (int i = 9; i >= 1; i--)
-        {
-            fps_history[i] = fps_history[i - 1];
-        }
-        fps_history[0] = fps;
-
-        if (fps_history[9] == 0.f)
-        {
-            return 0;
-        }
-
-        for (int i = 0; i < 10; i++)
-        {
-            avg_fps += fps_history[i];
-        }
-        avg_fps /= 10.f;
+    double t1 = ncnn::get_current_time();
+    if (t0 == 0.f) {
+      t0 = t1;
+      return 0;
     }
 
-    char text[32];
-    sprintf(text, "FPS=%.2f", avg_fps);
+    float fps = 1000.f / (t1 - t0);
+    t0 = t1;
 
-    int baseLine = 0;
-    cv::Size label_size = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
+    for (int i = 9; i >= 1; i--) {
+      fps_history[i] = fps_history[i - 1];
+    }
+    fps_history[0] = fps;
 
-    int y = 0;
-    int x = rgb.cols - label_size.width;
+    if (fps_history[9] == 0.f) {
+      return 0;
+    }
 
-    cv::rectangle(rgb, cv::Rect(cv::Point(x, y), cv::Size(label_size.width, label_size.height + baseLine)),
-                    cv::Scalar(255, 255, 255), -1);
+    for (int i = 0; i < 10; i++) {
+      avg_fps += fps_history[i];
+    }
+    avg_fps /= 10.f;
+  }
 
-    cv::putText(rgb, text, cv::Point(x, y + label_size.height),
-                cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0));
+  char text[32];
+  sprintf(text, "FPS=%.2f", avg_fps);
 
-    return 0;
+  int baseLine = 0;
+  cv::Size label_size = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
+
+  int y = 0;
+  int x = rgb.cols - label_size.width;
+
+  cv::rectangle(rgb,
+                cv::Rect(cv::Point(x, y), cv::Size(label_size.width, label_size.height + baseLine)),
+                cv::Scalar(255, 255, 255), -1);
+
+  cv::putText(rgb, text, cv::Point(x, y + label_size.height), cv::FONT_HERSHEY_SIMPLEX, 0.5,
+              cv::Scalar(0, 0, 0));
+
+  return 0;
 }
 
 static NanoDet* g_nanodet = 0;
 static ncnn::Mutex lock;
 
-class MyNdkCamera : public NdkCameraWindow
-{
-public:
-    virtual void on_image_render(cv::Mat& rgb) const;
+class MyNdkCamera : public NdkCameraWindow {
+ public:
+  virtual void on_image_render(cv::Mat& rgb) const;
 };
 
-void MyNdkCamera::on_image_render(cv::Mat& rgb) const
-{
-    // nanodet
-    {
-        ncnn::MutexLockGuard g(lock);
+void MyNdkCamera::on_image_render(cv::Mat& rgb) const {
+  // nanodet
+  {
+    ncnn::MutexLockGuard g(lock);
 
-        if (g_nanodet)
-        {
-            std::vector<Object> objects;
-            g_nanodet->detect(rgb, objects);
+    if (g_nanodet) {
+      std::vector<Object> objects;
+      g_nanodet->detect(rgb, objects);
 
-            g_nanodet->draw(rgb, objects);
-        }
-        else
-        {
-            draw_unsupported(rgb);
-        }
+      g_nanodet->draw(rgb, objects);
+    } else {
+      draw_unsupported(rgb);
     }
+  }
 
-    draw_fps(rgb);
+  draw_fps(rgb);
 }
 
 static MyNdkCamera* g_camera = 0;
 
 extern "C" {
 
-JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved)
-{
-    __android_log_print(ANDROID_LOG_DEBUG, "ncnn", "JNI_OnLoad");
+JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
+  __android_log_print(ANDROID_LOG_DEBUG, "ncnn", "JNI_OnLoad");
 
-    g_camera = new MyNdkCamera;
+  g_camera = new MyNdkCamera;
 
-    return JNI_VERSION_1_4;
+  return JNI_VERSION_1_4;
 }
 
-JNIEXPORT void JNI_OnUnload(JavaVM* vm, void* reserved)
-{
-    __android_log_print(ANDROID_LOG_DEBUG, "ncnn", "JNI_OnUnload");
+JNIEXPORT void JNI_OnUnload(JavaVM* vm, void* reserved) {
+  __android_log_print(ANDROID_LOG_DEBUG, "ncnn", "JNI_OnUnload");
 
-    {
-        ncnn::MutexLockGuard g(lock);
+  {
+    ncnn::MutexLockGuard g(lock);
 
-        delete g_nanodet;
-        g_nanodet = 0;
-    }
+    delete g_nanodet;
+    g_nanodet = 0;
+  }
 
-    delete g_camera;
-    g_camera = 0;
+  delete g_camera;
+  g_camera = 0;
 }
 
 // public native boolean loadModel(AssetManager mgr, int modelid, int cpugpu);
-JNIEXPORT jboolean JNICALL Java_com_tencent_nanodetncnn_NanoDetNcnn_loadModel(JNIEnv* env, jobject thiz, jobject assetManager, jint modelid, jint cpugpu)
-{
-    if (modelid < 0 || modelid > 6 || cpugpu < 0 || cpugpu > 1)
-    {
-        return JNI_FALSE;
+JNIEXPORT jboolean JNICALL Java_com_tencent_nanodetncnn_NanoDetNcnn_loadModel(
+    JNIEnv* env, jobject thiz, jobject assetManager, jint modelid, jint cpugpu) {
+  if (modelid < 0 || modelid > 6 || cpugpu < 0 || cpugpu > 1) {
+    return JNI_FALSE;
+  }
+
+  AAssetManager* mgr = AAssetManager_fromJava(env, assetManager);
+
+  __android_log_print(ANDROID_LOG_DEBUG, "ncnn", "loadModel %p", mgr);
+
+  const char* modeltypes[] = {"m",          "m-416",        "g", "ELite0_320", "ELite1_416",
+                              "ELite2_512", "RepVGG-A0_416"
+
+  };
+
+  const int target_sizes[] = {320, 416, 416, 320, 416, 512, 416};
+
+  const float mean_vals[][3] = {{103.53f, 116.28f, 123.675f}, {103.53f, 116.28f, 123.675f},
+                                {103.53f, 116.28f, 123.675f}, {127.f, 127.f, 127.f},
+                                {127.f, 127.f, 127.f},        {127.f, 127.f, 127.f},
+                                {103.53f, 116.28f, 123.675f}};
+
+  const float norm_vals[][3] = {
+      {1.f / 57.375f, 1.f / 57.12f, 1.f / 58.395f}, {1.f / 57.375f, 1.f / 57.12f, 1.f / 58.395f},
+      {1.f / 57.375f, 1.f / 57.12f, 1.f / 58.395f}, {1.f / 128.f, 1.f / 128.f, 1.f / 128.f},
+      {1.f / 128.f, 1.f / 128.f, 1.f / 128.f},      {1.f / 128.f, 1.f / 128.f, 1.f / 128.f},
+      {1.f / 57.375f, 1.f / 57.12f, 1.f / 58.395f}};
+
+  const char* modeltype = modeltypes[(int)modelid];
+  int target_size = target_sizes[(int)modelid];
+  bool use_gpu = (int)cpugpu == 1;
+
+  // reload
+  {
+    ncnn::MutexLockGuard g(lock);
+
+    if (use_gpu && ncnn::get_gpu_count() == 0) {
+      // no gpu
+      delete g_nanodet;
+      g_nanodet = 0;
+    } else {
+      if (!g_nanodet) g_nanodet = new NanoDet;
+      g_nanodet->load(mgr, modeltype, target_size, mean_vals[(int)modelid], norm_vals[(int)modelid],
+                      use_gpu);
     }
+  }
 
-    AAssetManager* mgr = AAssetManager_fromJava(env, assetManager);
-
-    __android_log_print(ANDROID_LOG_DEBUG, "ncnn", "loadModel %p", mgr);
-
-    const char* modeltypes[] =
-    {
-        "m",
-        "m-416",
-        "g",
-        "ELite0_320",
-        "ELite1_416",
-        "ELite2_512",
-        "RepVGG-A0_416"
-    };
-
-    const int target_sizes[] =
-    {
-        320,
-        416,
-        416,
-        320,
-        416,
-        512,
-        416
-    };
-
-    const float mean_vals[][3] =
-    {
-        {103.53f, 116.28f, 123.675f},
-        {103.53f, 116.28f, 123.675f},
-        {103.53f, 116.28f, 123.675f},
-        {127.f, 127.f, 127.f},
-        {127.f, 127.f, 127.f},
-        {127.f, 127.f, 127.f},
-        {103.53f, 116.28f, 123.675f}
-    };
-
-    const float norm_vals[][3] =
-    {
-        {1.f / 57.375f, 1.f / 57.12f, 1.f / 58.395f},
-        {1.f / 57.375f, 1.f / 57.12f, 1.f / 58.395f},
-        {1.f / 57.375f, 1.f / 57.12f, 1.f / 58.395f},
-        {1.f / 128.f, 1.f / 128.f, 1.f / 128.f},
-        {1.f / 128.f, 1.f / 128.f, 1.f / 128.f},
-        {1.f / 128.f, 1.f / 128.f, 1.f / 128.f},
-        {1.f / 57.375f, 1.f / 57.12f, 1.f / 58.395f}
-    };
-
-    const char* modeltype = modeltypes[(int)modelid];
-    int target_size = target_sizes[(int)modelid];
-    bool use_gpu = (int)cpugpu == 1;
-
-    // reload
-    {
-        ncnn::MutexLockGuard g(lock);
-
-        if (use_gpu && ncnn::get_gpu_count() == 0)
-        {
-            // no gpu
-            delete g_nanodet;
-            g_nanodet = 0;
-        }
-        else
-        {
-            if (!g_nanodet)
-                g_nanodet = new NanoDet;
-            g_nanodet->load(mgr, modeltype, target_size, mean_vals[(int)modelid], norm_vals[(int)modelid], use_gpu);
-        }
-    }
-
-    return JNI_TRUE;
+  return JNI_TRUE;
 }
 
 // public native boolean openCamera(int facing);
-JNIEXPORT jboolean JNICALL Java_com_tencent_nanodetncnn_NanoDetNcnn_openCamera(JNIEnv* env, jobject thiz, jint facing)
-{
-    if (facing < 0 || facing > 1)
-        return JNI_FALSE;
+JNIEXPORT jboolean JNICALL Java_com_tencent_nanodetncnn_NanoDetNcnn_openCamera(JNIEnv* env,
+                                                                               jobject thiz,
+                                                                               jint facing) {
+  if (facing < 0 || facing > 1) return JNI_FALSE;
 
-    __android_log_print(ANDROID_LOG_DEBUG, "ncnn", "openCamera %d", facing);
+  __android_log_print(ANDROID_LOG_DEBUG, "ncnn", "openCamera %d", facing);
 
-    g_camera->open((int)facing);
+  g_camera->open((int)facing);
 
-    return JNI_TRUE;
+  return JNI_TRUE;
 }
 
 // public native boolean closeCamera();
-JNIEXPORT jboolean JNICALL Java_com_tencent_nanodetncnn_NanoDetNcnn_closeCamera(JNIEnv* env, jobject thiz)
-{
-    __android_log_print(ANDROID_LOG_DEBUG, "ncnn", "closeCamera");
+JNIEXPORT jboolean JNICALL Java_com_tencent_nanodetncnn_NanoDetNcnn_closeCamera(JNIEnv* env,
+                                                                                jobject thiz) {
+  __android_log_print(ANDROID_LOG_DEBUG, "ncnn", "closeCamera");
 
-    g_camera->close();
+  g_camera->close();
 
-    return JNI_TRUE;
+  return JNI_TRUE;
 }
 
 // public native boolean setOutputWindow(Surface surface);
-JNIEXPORT jboolean JNICALL Java_com_tencent_nanodetncnn_NanoDetNcnn_setOutputWindow(JNIEnv* env, jobject thiz, jobject surface)
-{
-    ANativeWindow* win = ANativeWindow_fromSurface(env, surface);
+JNIEXPORT jboolean JNICALL Java_com_tencent_nanodetncnn_NanoDetNcnn_setOutputWindow(
+    JNIEnv* env, jobject thiz, jobject surface) {
+  ANativeWindow* win = ANativeWindow_fromSurface(env, surface);
 
-    __android_log_print(ANDROID_LOG_DEBUG, "ncnn", "setOutputWindow %p", win);
+  __android_log_print(ANDROID_LOG_DEBUG, "ncnn", "setOutputWindow %p", win);
 
-    g_camera->set_window(win);
+  g_camera->set_window(win);
 
-    return JNI_TRUE;
+  return JNI_TRUE;
 }
-
 }
